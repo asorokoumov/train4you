3e3cfb078e6ef50d04ba01e0ef0f5030dfeb47a1
Sorokoumov Anton <anton.sorokoumov@lamoda.ru>
2015-01-23 18:39:54 +0300

    Added 2 urls to receive w1 request

--- app/controllers/w1_controller.rb
+++ app/controllers/w1_controller.rb
@@ -2,17 +2,14 @@ class W1Controller < ApplicationController
   protect_from_forgery :except=>[:w1_return]
 
 
- def w1_return
-  #  @post = Post.new(data_json)
-#  if @post.save
-
-
-
-# data_json = JSON.parse request.body.read
-
- render text: "#{request.body.read}"
-
-
- # render text: "#{data_json['test1']}"
+ def w1_return_success
+ # data_json = JSON.parse request.body.read
+ #render text: "#{request.body.read}"
+ render text: "WMI_RESULT=OK"
+ end
+ def w1_return_fail
+ # data_json = JSON.parse request.body.read
+ #render text: "#{request.body.read}"
+  render text: "WMI_RESULT=OK"
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 root to: 'application#index'
-match '/payment-backend'=>'w1#w1_return', :via => [:post]
-
+match '/payment-sucess'=>'w1#w1_return_success', :via => [:post]
+match '/payment-fail'=>'w1#w1_return_fail', :via => [:post]
 
 end
-
+	
 
